76ef586fc33a536da9fbe587dc8596109fefda85
Sai Teja Pratap <pratap130492@gmail.com>
2017-04-15 12:49:06 -0700

    Modularize pojo gen

--- src/main/java/in/suram/protobuf/PojoGen.java
+++ src/main/java/in/suram/protobuf/PojoGen.java
@@ -17,8 +17,6 @@ public class PojoGen {
 
   public static String Generate(Message message, Config config)
       throws IOException, FormatterException {
-    StringBuilder pojo = new StringBuilder();
-    StringBuilder pojoBuilder = new StringBuilder();
 
     // ==pojoBuilder==
     // |  public class Builder {;
@@ -60,46 +58,20 @@ public class PojoGen {
     // |    if (_string_rep != null) { return _string_rep; }
     // |    _string_rep = GenerateStringRep();
     // |  }
-    pojo.append("public class ").append(message.name).append(" {\n");
-    pojo.append("  ").append("private String _string_rep;\n");
-    pojoBuilder.append("  public static class Builder").append(" {\n");
-    // Field declarations.
 
-    for (Message.Field field : message.fields) {
-      String type = field.type.javaType;
-      String name = field.javaName;
-      String pojoLine = format("public final %s %s;", type, name);
-      String pojoBuilderLine = format("private %s %s;", type, name);
-      pojo.append("  ").append(pojoLine).append("\n");
-      pojoBuilder.append("    ").append(pojoBuilderLine).append("\n");
-    }
+    StringBuilder pojoBuilder = new StringBuilder();
 
-    // pojoBuilder setters and getters.
-    for (Message.Field field : message.fields) {
-      String type = field.type.javaType;
-      String name = Utils.lowerCamel(field.name);
-      String getter =
-          format("public %s %s() { return this.%s; }", type, field.javaGetterName, name);
-      String setter =
-          format(
-              "public Builder %s(%s %s) { this.%s = %s; return this; }",
-              field.javaSetterName, type, name, name, name);
-      pojoBuilder.append("    ").append(getter).append("\n");
-      pojoBuilder.append("    ").append(setter).append("\n");
-    }
+    pojoBuilder.append("public static class Builder").append("{\n");
+    appendFieldDeclarations(pojoBuilder, message, "private");
+    gettersAndSetters(message, pojoBuilder);
+    buildFunction(message, pojoBuilder);
+    pojoBuilder.append("}").append("\n");
 
-    pojoBuilder.append("    ").append(format("public %s build() {", message.name)).append("\n");
-    pojoBuilder.append("      ").append(format("return new %s(", message.name)).append("\n");
-    for (Message.Field field : message.fields) {
-      pojoBuilder.append("               ").append(field.name).append(",").append("\n");
-    }
-    Utils.deleteLastX(pojoBuilder, 2).append(");").append("\n");
-    pojoBuilder.append("    }").append("\n");
-
-    pojoBuilder.append("  ").append("}").append("\n");
-
-    pojo.append(getConstructor(message));
-    pojo.append(getToStringFunction(message));
+    StringBuilder pojo = new StringBuilder();
+    pojo.append("public class ").append(message.name).append("{\n");
+    appendFieldDeclarations(pojo, message, "public final");
+    appendConstructor(message, pojo);
+    appendToStringFunction(message, pojo);
     pojo.append(pojoBuilder);
     pojo.append("}").append("\n");
     // |package ${pkg};
@@ -111,38 +83,83 @@ public class PojoGen {
     return new Formatter().formatSource(sourceString);
   }
 
-  private static String getToStringFunction(Message message) {
-    StringBuilder toStringFunc = new StringBuilder();
-    toStringFunc.append("  ").append("private String GenerateStringRep() {").append("\n");
-    toStringFunc.append("     ").append("return MoreObjects.toStringHelper(this).").append("\n");
+  private static StringBuilder gettersAndSetters(Message message, StringBuilder pojoBuilder) {
+    // pojoBuilder setters and getters.
     for (Message.Field field : message.fields) {
-      String addFieldToString = format("add(\"%s\", %s).", field.javaName, field.javaName);
-      toStringFunc.append("              ").append(addFieldToString).append("\n");
+      String type = field.type.javaType;
+      String name = Utils.lowerCamel(field.name);
+      String getter =
+          format("public %s %s() { return this.%s; }", type, field.javaGetterName, name);
+      String setter =
+          format(
+              "public Builder %s(%s %s) { this.%s = %s; return this; }",
+              field.javaSetterName, type, name, name, name);
+      pojoBuilder.append(getter).append("\n");
+      pojoBuilder.append(setter).append("\n");
     }
-
-    toStringFunc.append("              ").append("toString();").append("\n");
-    toStringFunc.append("  ").append("}").append("\n");
-    toStringFunc.append("  ").append("@Override").append("\n");
-    toStringFunc.append("  ").append("public String toString() {").append("\n");
-    String setStringRepIfNull = "if (_string_rep == null) { _string_rep = GenerateStringRep(); }";
-    toStringFunc.append("    ").append(setStringRepIfNull).append("\n");
-    toStringFunc.append("    ").append("return _string_rep;").append("\n");
-    toStringFunc.append("  ").append("}").append("\n");
-    return toStringFunc.toString();
+    return pojoBuilder;
   }
 
-  private static String getConstructor(Message message) {
-    StringBuilder constructor = new StringBuilder("private ").append(message.name).append("(");
+  private static StringBuilder buildFunction(Message message, StringBuilder pojoBuilder) {
+    pojoBuilder.append(format("public %s build() {", message.name)).append("\n");
+    pojoBuilder.append(format("return new %s(", message.name)).append("\n");
     for (Message.Field field : message.fields) {
-      constructor.append(field.type.javaType).append(" ").append(field.javaName).append(",");
+      pojoBuilder.append(field.name).append(",").append("\n");
     }
-    Utils.deleteLastX(constructor, 1).append(") {");
+    Utils.deleteLastX(pojoBuilder, 2).append(");").append("\n");
+    pojoBuilder.append("}").append("\n");
+    return pojoBuilder;
+  }
+
+  private static StringBuilder appendFieldDeclarations(
+      StringBuilder toWrite, Message message, String fieldModifiers) {
+    for (Message.Field field : message.fields) {
+      // {fieldModifier} {fieldType} {fieldType};
+      // E.g private static int id;
+      toWrite
+          .append(fieldModifiers)
+          .append(" ")
+          .append(field.type.javaType)
+          .append(" ")
+          .append(field.javaName)
+          .append(";\n");
+    }
+    return toWrite;
+  }
+
+  private static StringBuilder appendToStringFunction(Message message, StringBuilder pojo) {
+    pojo.append("private String _string_rep;\n");
+    pojo.append("private String GenerateStringRep() {").append("\n");
+    pojo.append("return MoreObjects.toStringHelper(this).").append("\n");
+    for (Message.Field field : message.fields) {
+      String addFieldToString = format("add(\"%s\", %s).", field.javaName, field.javaName);
+      pojo.append(addFieldToString).append("\n");
+    }
+
+    pojo.append("toString();").append("\n");
+    pojo.append("}").append("\n");
+    pojo.append("@Override").append("\n");
+    pojo.append("public String toString() {").append("\n");
+    String setStringRepIfNull = "if (_string_rep == null) { _string_rep = GenerateStringRep(); }";
+    pojo.append(setStringRepIfNull).append("\n");
+    pojo.append("return _string_rep;").append("\n");
+    pojo.append("}").append("\n");
+    return pojo;
+  }
+
+  private static StringBuilder appendConstructor(Message message, StringBuilder pojo) {
+    pojo.append("private ").append(message.name).append("(");
+    for (Message.Field field : message.fields) {
+      pojo.append(field.type.javaType).append(" ").append(field.javaName).append(",");
+    }
+    Utils.deleteLastX(pojo, 1).append(") {");
     for (Message.Field field : message.fields) {
       String name = field.javaName;
-      constructor.append(format("this.%s = %s;", name, name));
+      pojo.append(format("this.%s = %s;", name, name));
     }
-    constructor.append("}");
-    return constructor.toString();
+    pojo.append("}");
+    pojo.append("\n");
+    return pojo;
   }
 
   public static void main(String[] args) throws IOException, FormatterException {
@@ -156,7 +173,7 @@ public class PojoGen {
       System.out.printf("name=%s\n", message.name);
       for (Message.Field field : message.fields) {
         System.out.printf(
-            " Field = `%s` type = `%s` rule = `%s` id = `%s`\n",
+            "Field = `%s` type = `%s` rule = `%s` id = `%s`\n",
             field.name, field.type, field.fieldRule, field.id);
       }
       System.out.println("\n==Generated Code==\n");
